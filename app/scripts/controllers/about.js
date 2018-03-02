713d2bb791ce5ea78e8cb73fb1a89087996294e5
sandra juarez <drasan_mar@hotmail.com>
2018-03-01 19:01:23 -0600

    primer commit en vacante

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name examenVacanteApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the examenVacanteApp
+ */
+angular.module('examenVacanteApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name examenVacanteApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the examenVacanteApp
+ */
+angular.module('examenVacanteApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
